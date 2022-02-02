d3da4379bf0de83f1a045c73fb2d9993a4886844
uduma sonia <sohnyauduma@gmail.com>
2022-02-02 23:58:05 +0100

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,14 +3,14 @@ import React from "react";
 function Navbar() {
   return (
     <div className="border-b border-slate-300 h-32 text-slate-800 text-md font-bold">
-      <ul className="uppercase flex justify-end mt-10">
+      <ul className="uppercase flex justify-end items-end mt-10">
         <li>trips</li>
 
         <li className="mx-12">recently viewed</li>
 
         <li className="mr-12">bookings</li>
 
-        <li className="profile_picture">picture</li>
+        <li className="profile_picture h-10 w-10 bg-red-700 rounded-full"></li>
       </ul>
     </div>
   );
